ad2f0228e2c940075d3553f18f47f7ac4c11d5d6
zoverlvx <zoverhulser@gmail.com>
2018-04-10 17:11:56 -0400

    Greets user by name in Spanish.

--- index.mjs
+++ index.mjs
@@ -1,7 +1,9 @@
 export function makeGreeting(name = "world", language = "English") {
     if (language === "Spanish") {
-        const world = "mundo";
-        return "\u00A1Hola " + world + "!";
+        if (name === "") { 
+            const world = "mundo";
+            return "\u00A1Hola " + world + "!";
+        }
     }
     return "Hello " + name + "!";
 }
