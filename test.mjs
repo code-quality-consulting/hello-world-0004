8f8b7ad17bc8b6573edf474c4793e2adfce3b624
zoverlvx <zoverhulser@gmail.com>
2018-04-10 18:32:19 -0400

    Tests Spanish greeting with partial application.

--- test.mjs
+++ test.mjs
@@ -2,10 +2,11 @@ import assert from "assert";
 import {makeGreeting, makeGreeter} from "./index"
 
 const makeEnglishGreeting = makeGreeter("English");
+const makeSpanishGreeting = makeGreeter("Spanish");
 
 assert.equal(makeEnglishGreeting(), "Hello world!");
 assert.equal(makeEnglishGreeting("Ben"), "Hello Ben!");
-assert.equal(makeGreeting("", "Spanish"), "\u00A1Hola mundo!");
+assert.equal(makeSpanishGreeting(), "\u00A1Hola mundo!");
 assert.equal(makeGreeting("Edgar", "Spanish"), "\u00A1Hola Edgar!")
 console.log("success!");
 
