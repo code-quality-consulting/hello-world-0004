6f256c91fc2800c2d1591891bb69fa45b97ec12e
zoverlvx <zoverhulser@gmail.com>
2018-04-10 16:40:21 -0400

    Greets world in Spanish.

--- index.mjs
+++ index.mjs
@@ -1,3 +1,7 @@
-export function makeGreeting(name = "world") {
+export function makeGreeting(name = "world", language) {
+    if (language === "Spanish") {
+        name = "mundo";
+        return "\u00A1Hola " + name + "!";
+    }
     return "Hello " + name + "!";
 }
