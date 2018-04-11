c05eca188b702732f1ec56a1afe6532ea8084dc0
zoverlvx <zoverhulser@gmail.com>
2018-04-10 20:43:09 -0400

    Removes makeGreeting().

--- index.mjs
+++ index.mjs
@@ -1,14 +1,3 @@
-export function makeGreeting(name = "world", language = "English") {
-    if (language === "Spanish") {
-        if (name === "") { 
-            const world = "mundo";
-            return "\u00A1Hola " + world + "!";
-        }
-        return "\u00A1Hola " + name + "!";
-    }
-    return "Hello " + name + "!";
-}
-
 export function makeGreeter(language) {
     if (language === "Spanish") {
         return function (name = "mundo") {
