f96d1aa258c85ff41c0e11440483d26758c8c9fd
zoverlvx <zoverhulser@gmail.com>
2018-04-17 19:08:02 -0400

    Substitutes 'greeting' and 'world' via global dictionary array.

--- index.mjs
+++ index.mjs
@@ -1,15 +1,33 @@
+const dictionary = [
+    {
+        language: "English",
+        greeting: "Hello",
+        world: "world"
+    },
+    {
+        language: "Spanish",
+        greeting: "\u00A1Hola",
+        world: "mundo"
+    },
+    {
+        language: "Russian",
+        greeting: "Привет",
+        world: "мир"
+    }
+];
+
 export function makeGreeter(language) {
     if (language === "Spanish") {
-        return function (name = "mundo") {
-            return "\u00A1Hola " + name + "!";
+        return function (name = dictionary[1].world) {
+            return dictionary[1].greeting + " " + name + "!";
         };
     }
     if (language === "Russian") {
-        return function (name = "мир") {
-            return "Привет " + name + "!";
+        return function (name = dictionary[2].world) {
+            return dictionary[2].greeting + " " + name + "!";
         };
     }
-    return function (name = "world") {
-        return "Hello " + name + "!";
+    return function (name = dictionary[0].world) {
+        return dictionary[0].greeting + " " +  name + "!";
     };
 }
