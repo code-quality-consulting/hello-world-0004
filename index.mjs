af86b91a4f7e47a810ee2b03fa5ae9b8dc3b510b
zoverlvx <zoverhulser@gmail.com>
2018-04-17 18:18:11 -0400

    Greets user in Russian.

--- index.mjs
+++ index.mjs
@@ -4,6 +4,11 @@ export function makeGreeter(language) {
             return "\u00A1Hola " + name + "!";
         };
     }
+    if (language === "Russian") {
+        return function (name = "мир") {
+            return "Привет " + name + "!";
+        };
+    }
     return function (name = "world") {
         return "Hello " + name + "!";
     };
